9718bb9185bc599430aed789367aa347e224fa52
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-11-14 19:43:16 +0530

    Part 7: Streamlining Your Next.js App with a Custom App Component

---
+++ next-blogs/pages/_app.js
@@ -0,0 +1,15 @@
+// File: pages/_app.js
+import NavBar from '../components/NavBar';
+
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default App;
--- next-blogs/pages/about.js
+++ next-blogs/pages/about.js
@@ -1,18 +1,11 @@
 // File: pages/about.js
 import NavBar from '../components/NavBar';
-
 function AboutPage() {
   console.log('[AboutPage] render');
   return (
-    <>
-      <header>
-        <NavBar />
-      </header>
-      <main>
-        <h1>About</h1>
-      </main>
-    </>
+    <main>
+      <h1>About</h1>
+    </main>
   );
 }
-
 export default AboutPage;
