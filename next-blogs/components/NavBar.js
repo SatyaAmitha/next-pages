5e7aac71ca8572ad4186504e7c572806140ceca8
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-11-14 20:02:22 +0530

    Part 10: Elevating Your Next.js Application with Component-Scoped Styles

--- next-blogs/components/NavBar.js
+++ next-blogs/components/NavBar.js
@@ -1,21 +1,28 @@
 // File: components/NavBar.js
 import Link from 'next/link';
-
 function NavBar() {
   return (
     <nav>
       <ul>
         <li>
-          <Link href="/">
-            Home
-          </Link>
+          <Link href="/">Home</Link>
         </li>
         <li>
-          <Link href="/about">
-            About
-          </Link>
+          <Link href="/about">About</Link>
         </li>
       </ul>
+      <style jsx>{`
+        ul {
+          list-style-type: none;
+          padding: 0;
+        }
+        li {
+          display: inline;
+        }
+        li:not(:first-child) {
+          margin-left: 0.75rem;
+        }
+      `}</style>
     </nav>
   );
 }
--- next-blogs/pages/index.js
+++ next-blogs/pages/index.js
@@ -3,16 +3,21 @@ import Link from 'next/link';
 import NavBar from '../components/NavBar';
 import Head from 'next/head';
 function HomePage() {
-  console.log('[HomePage] render');
   return (
     <>
       <Head>
         <title>My Blog</title>
       </Head>
-    <main>
-      <h1>My Blog</h1>
-    </main>
+      <main>
+        <h1>My Blog</h1>
+        <ul>
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </ul>
+      </main>
     </>
   );
 }
+
 export default HomePage;
