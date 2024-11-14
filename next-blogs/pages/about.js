07bebea1422e8e7b57dbf628d8cd70d11b371200
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-11-14 19:34:08 +0530

    Part 4: Creating Multiple Pages in a Next.js Application: Adding an "About" Page

---
+++ next-blogs/pages/about.js
@@ -0,0 +1,11 @@
+// File: pages/about.js
+function AboutPage() {
+  console.log('[AboutPage] render');
+  return (
+    <main>
+      <h1>About</h1>
+    </main>
+  );
+}
+
+export default AboutPage;
