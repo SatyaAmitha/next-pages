e9199338e5c4af085249cff0a67c93a9a3c7159d
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-11-15 11:45:46 +0530

    Part 21: Automating Blog Post Paths in Next.js

--- next-blogs/lib/posts.js
+++ next-blogs/lib/posts.js
@@ -1,8 +1,13 @@
 // lib/posts.js
-import { readFile } from 'fs/promises';
+import { readdir,readFile } from 'fs/promises';
 import matter from 'gray-matter';
 import { marked } from 'marked';
-
+export async function getSlugs() {
+  const suffix = '.md';
+  const files = await readdir(`content/posts`);
+  return files.filter((file) => file.endsWith(suffix))
+    .map((file) => file.slice(0, -suffix.length));
+}
 export async function getPost(slug) {
   const source = await readFile(`content/posts/${slug}.md`, 'utf8');
   const { data: { date, title }, content } = matter(source);
