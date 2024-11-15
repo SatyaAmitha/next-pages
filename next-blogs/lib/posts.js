a67b664cfb69d473836fa7530c737b2e83dd33e6
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-11-15 10:27:18 +0530

    Part 17: Refactoring getStaticProps for Better Code Organization in Next.js

---
+++ next-blogs/lib/posts.js
@@ -0,0 +1,7 @@
+// lib/posts.js
+import { readFile } from 'fs/promises';
+
+export async function getPost(slug) {
+  const data = await readFile(`content/posts/${slug}.json`, 'utf8');
+  return JSON.parse(data);
+}
--- next-blogs/pages/posts/first-post.js
+++ next-blogs/pages/posts/first-post.js
@@ -1,11 +1,10 @@
 // pages/posts/first-post.js
-import { readFile } from 'fs/promises';
 import Head from 'next/head';
+import { getPost } from '../../lib/posts';
 
 export async function getStaticProps() {
   console.log('[FirstPostPage] getStaticProps()');
-  const data = await readFile('content/posts/first-post.json', 'utf8');
-  const post = JSON.parse(data);
+  const post = await getPost('first-post');
   return {
     props: { post },
   };
