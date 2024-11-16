1d56dbfced76a58e82845e62cd97ee0c2bcd55d7
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-11-16 12:17:02 +0530

    Part 28: Enhancing User Experience with Local Storage in Next.js

--- next-blogs/components/ThemeSwitch.js
+++ next-blogs/components/ThemeSwitch.js
@@ -1,15 +1,29 @@
 // components/ThemeSwitch.js
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import DarkTheme from './DarkTheme';
 
 function ThemeSwitch() {
   const [darkMode, setDarkMode] = useState(false);
 
+  useEffect(() => {
+    const value = localStorage.getItem('darkMode');
+    if (value !== null) {
+      setDarkMode(JSON.parse(value));
+    }
+  }, []);
+
+  const handleClick = () => {
+    localStorage.setItem('darkMode', JSON.stringify(!darkMode));
+    setDarkMode(!darkMode);
+  };
+
+  const text = darkMode ? 'Light Mode' : 'Dark Mode';
+
   return (
     <>
-      <button onClick={() => setDarkMode(!darkMode)}>
-        {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+      <button onClick={handleClick}>
+        {text}
       </button>
       <style jsx>{`
         button {
